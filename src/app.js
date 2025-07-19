eb9c46f40c3cf16cf295756e5f26867dc370c916
Abhi Ram Dathu <lingarajuvelishala11@gmail.com>
2025-07-19 11:24:38 +0530

    fix all register controller bugs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+// routes
+import userRoutes from './routes/user.routes.js';
+
 const app =  express()
 
 app.use(cors( {
@@ -14,12 +17,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-// routes import
 
-import userRouter from './routes/user.routes.js'
 
 //routes declaration
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRoutes)
 
 // http://localhost:8000/api/v1/users/register
 export { app }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@ import fs from "fs" //(fs = file system);
 cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: CLOUDINARY_API_SECRET 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
     });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -17,7 +17,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been aploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
+        //console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
         
     } catch (error) {
